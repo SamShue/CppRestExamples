5316ca5fc1e19998bb291e83183d3d312ada522e
sam.shue <sam.shue@iamrobotics.com>
2020-07-08 13:44:02 -0400

    Added project files and cmake file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "restServer.h"
+
+using namespace std;
+
+int main(){
+
+    RestServer restServer;
+
+    return 0;
+}
---
+++ restServer.h
@@ -0,0 +1,17 @@
+#ifndef REST_SERVER_H
+#define REST_SERVER_H
+
+#include <cpprest/asyncrt_utils.h>
+//#include <cpprest/http_listener.h>
+//#include <cpprest/json.h>
+//#include <cpprest/uri.h>
+
+class RestServer{
+    public:
+
+    private:
+    //web::http::experimental::listener::http_listener m_listener;
+};
+
+
+#endif
